2da97ca4369d9353a18dfd683d32774eab82683f
Bruno Feres Villela <brunoferesvillela@icloud.com>
2021-04-04 14:01:46 -0300

    Update

--- components/FileUploader/index.tsx
+++ components/FileUploader/index.tsx
@@ -26,6 +26,11 @@ export const FileUploader: React.FC<Props> = ({ setPreview, preview }) => {
     }
   }
 
+  const removePhoto = (blob) => {
+    let _imagesToShow = imagesToShow.filter((item) => item !== blob)
+    setImagesToShow(_imagesToShow)
+  }
+
   const handleSetImage = (event: React.ChangeEvent<HTMLInputElement>) => {
     let files = event.target.files
     let filesForPreview = []
@@ -60,32 +65,38 @@ export const FileUploader: React.FC<Props> = ({ setPreview, preview }) => {
         />
       </label>
 
-      <Div onClick={handleUpdateImage} m={{ t: '1rem' }} bg='white' w='100%' h='10rem' d='flex' justify='center' align='center' cursor='pointer'>
-        <Center>
-          {
-            imagesToShow.length == 0 ?
-            <>
+      {
+        imagesToShow.length == 0 ?
+          <Div onClick={handleUpdateImage} m={{ t: '1rem' }} bg='white' w='100%' h='10rem' d='flex' justify='center' align='center' cursor='pointer'>
+            <Center>
               <Icon name='CameraSolid' size='40px' color='gray4' />
               <Text textColor='gray2'> {t('product.attributes.photos')} </Text>
-            </>
-            :
-            <Div d='flex' justify='center'>
-              {
-                imagesToShow.map((image, index) => {
-                  return(
-                    <Div m={{ x: '2%' }}>
-                      <Div rounded='5px' shadow='2' w='6rem' h='8rem' d='flex' justify='center' align='center' bg='gray6' overflow='hidden'>
-                        <Image src={image} alt='Product image' style={{width: '5rem'}} />
-                      </Div>
-                      <Icon m={{ t: '5%' }} name='Close' size='20px' color='red' />
+            </Center>
+          </Div>
+        :
+          <Div d='flex' justify='center'>
+            {
+              imagesToShow.map((image, index) => {
+                return(
+                  <Div m={{ x: '2%' }}>
+                    <Div rounded='5px' shadow='2' w='6rem' h='8rem' d='flex' justify='center' align='center' bg='gray6' overflow='hidden'>
+                      <Image src={image} alt='Product image' style={{width: '5rem'}} />
                     </Div>
-                  )
-                })
-              }
+                    <Center>
+                      <Icon onClick={() => removePhoto(image)} m={{ t: '5%' }} name='Close' size='20px' color='red' />
+                    </Center>
+                  </Div>
+                )
+              })
+            }
+
+            <Div m={{ x: '2%' }}>
+              <Div onClick={handleUpdateImage} rounded='5px' w='6rem' h='8rem' d='flex' justify='center' align='center' hoverShadow='3' cursor='pointer'>
+                <Icon name='CameraSolid' size='40px' color='gray4' />
+              </Div>
             </Div>
-          }
-        </Center>
-      </Div>
+          </Div>
+      }
     </Div>
   )
 }
